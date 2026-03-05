202b38835c3eb3a5b1c11a3a77f076582090da1d
Aditya Thakare <aadiiithakre2529@gmail.com>
2026-03-05 21:14:26 +0530

    Create Project folder

---
+++ 03_JS/script.js
@@ -0,0 +1,62 @@
+if(true){
+    let b=5;
+    // console.log(b)
+}
+// console.log(b)
+
+const pi = 3.14;
+/*
+
+// block scoped
+// cannot redeclared
+    cannot be updated
+    must initialized while decarling
+    
+
+
+*/
+/**
+ *  Feature         var             let             const
+ *  ---------       -----------     --------        --------
+ *  Scoped          Function        Block           Block
+ *  Redeclare       Yes             No              No
+ *  Update          Yes             Yes             No
+ *  Hoisting        Yes             yes (but TDZ)   Yes (but TDZ)
+ * 
+ * TDZ -> Temporary death zone
+ * Hoisting
+ *  
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+/**
+ * Data Types:-
+ *     Konse type ka data hum add kr rahe hai varaibles me batana
+ * JS is dnyamically typed
+ * this means you don't need to declare data types
+ * 
+ */
+
+let x=10;
+x="Aditya";
+console.log(x)
+
+// same variable can store different tyeps
+// types of data types
+// Primitive data types -> single value store
+/**
+ * stirng
+ * number
+ * undefined
+ * null
+ * boolean
+ */
+// Non premitive data types -> collection of data store
+/**
+ * array
+ * object
+ */
